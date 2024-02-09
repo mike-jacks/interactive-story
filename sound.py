ba42764b6e21b73ae50cb2cd218ff945f14e6bc8
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-02-09 13:49:38 -0700

    add sound.py

---
+++ sound.py
@@ -0,0 +1,15 @@
+from time import sleep
+
+class Sound:
+    @staticmethod
+    def play():
+        print("Playing sound")
+    
+    @staticmethod
+    def stop():
+        print("Stopping sound")
+    
+    @staticmethod
+    def loop(num_of_times: float):
+        print("Looping sound")
+    
