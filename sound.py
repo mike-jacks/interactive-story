92e729f5ad002806e21396a0e924eb6fe5c02ab1
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-02-09 14:04:31 -0700

    update sound.py with pysound and threading for playback

--- sound.py
+++ sound.py
@@ -2,10 +2,20 @@ from time import sleep
 import threading
 from playsound import playsound
 
+MAC_OS_ERROR_1_SOUND = 'sounds/mac_os_error_1.wav'
+MAC_OS_ERROR_2_SOUND = 'sounds/mac_os_error_2.wav'
+MAC_OS_NOTIFICAITON_1_SOUND = 'sounds/mac_os_notification_1.wav'
+MAC_OS_NOTIFICATION_2_SOUND = 'sounds/mac_os_notification_2.wav'
+MAC_OS_OLD_SCHOOL_ERROR_SOUND = 'sounds/mac_os_old_school_error.wav'
+MAC_OS_STARTUP_CLASSIC_SOUND = 'sounds/mac_os_startup_classic.wav'
+MAC_OS_STARTUP_MODERN_SOUND = 'sounds/mac_os_startup_modern.wav'
+
 class Sound:
     @staticmethod
-    def play():
-        print("Playing sound")
+    def play(sound_file: str):
+        def sound_thread():
+            playsound(sound_file)
+        threading.Thread(target=sound_thread).start()
     
     @staticmethod
     def stop():
@@ -14,4 +24,5 @@ class Sound:
     @staticmethod
     def loop(num_of_times: float):
         print("Looping sound")
+    
     
