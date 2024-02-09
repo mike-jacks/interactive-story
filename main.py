8d74c851b5fe89912dcefe71de6afe5e3efd2b9b
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-02-09 14:51:36 -0700

    update sound.py and main.py
    This commit updates sound.py to have a class with static methods to play
    a single sound file, defaulted to MAC OS STARTUP MODERN playing 1 time
    with 0 delay. Options available to lopp a certain number of times, with
    a certain speed/delay in between each sound play.
    
    main.py has been updated to test sound.py by playing a crash() method
    from Sound.

---
+++ main.py
@@ -0,0 +1,10 @@
+from sound import Sound
+from time import sleep
+import threading
+
+def main():
+    Sound.crash()
+    
+
+if __name__ == '__main__':
+    main()
--- sound.py
+++ sound.py
@@ -2,27 +2,74 @@ from time import sleep
 import threading
 from playsound import playsound
 
-MAC_OS_ERROR_1_SOUND = 'sounds/mac_os_error_1.wav'
-MAC_OS_ERROR_2_SOUND = 'sounds/mac_os_error_2.wav'
-MAC_OS_NOTIFICAITON_1_SOUND = 'sounds/mac_os_notification_1.wav'
-MAC_OS_NOTIFICATION_2_SOUND = 'sounds/mac_os_notification_2.wav'
-MAC_OS_OLD_SCHOOL_ERROR_SOUND = 'sounds/mac_os_old_school_error.wav'
-MAC_OS_STARTUP_CLASSIC_SOUND = 'sounds/mac_os_startup_classic.wav'
-MAC_OS_STARTUP_MODERN_SOUND = 'sounds/mac_os_startup_modern.wav'
-
 class Sound:
+    MAC_OS_ERROR_1_SOUND = 'sounds/mac_os_error_1.wav'
+    MAC_OS_ERROR_2_SOUND = 'sounds/mac_os_error_2.wav'
+    MAC_OS_NOTIFICAITON_1_SOUND = 'sounds/mac_os_notification_1.wav'
+    MAC_OS_NOTIFICATION_2_SOUND = 'sounds/mac_os_notification_2.wav'
+    MAC_OS_OLD_SCHOOL_ERROR_SOUND = 'sounds/mac_os_old_school_error.wav'
+    MAC_OS_STARTUP_CLASSIC_SOUND = 'sounds/mac_os_startup_classic.wav'
+    MAC_OS_STARTUP_MODERN_SOUND = 'sounds/mac_os_startup_modern.wav'
+    
+    
     @staticmethod
-    def play(sound_file: str):
+    def play(sound_file: str = MAC_OS_STARTUP_MODERN_SOUND, num_of_times: int = 1, speed: float = 0.0):
         def sound_thread():
             playsound(sound_file)
-        threading.Thread(target=sound_thread).start()
+        for i in range(num_of_times):
+            threading.Thread(target=sound_thread).start()
+            sleep(speed)
     
     @staticmethod
     def stop():
         print("Stopping sound")
     
     @staticmethod
-    def loop(num_of_times: float):
-        print("Looping sound")
-    
-    
+    def crash():
+        Sound.play(Sound.MAC_OS_STARTUP_CLASSIC_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_1_SOUND, 2, 0)
+        Sound.play(Sound.MAC_OS_ERROR_2_SOUND, 2, 0)
+        Sound.play(Sound.MAC_OS_OLD_SCHOOL_ERROR_SOUND, 2, 0)
+        Sound.play(Sound.MAC_OS_ERROR_1_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_2_SOUND)
+        Sound.play(Sound.MAC_OS_OLD_SCHOOL_ERROR_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_1_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_2_SOUND)
+        Sound.play(Sound.MAC_OS_OLD_SCHOOL_ERROR_SOUND)
+        Sound.play(Sound.MAC_OS_STARTUP_MODERN_SOUND, 2, 0)
+        Sound.play(Sound.MAC_OS_ERROR_1_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_2_SOUND)
+        Sound.play(Sound.MAC_OS_OLD_SCHOOL_ERROR_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_1_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_2_SOUND)
+        Sound.play(Sound.MAC_OS_OLD_SCHOOL_ERROR_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_1_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_2_SOUND)
+        Sound.play(Sound.MAC_OS_OLD_SCHOOL_ERROR_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_1_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_2_SOUND)
+        Sound.play(Sound.MAC_OS_STARTUP_CLASSIC_SOUND, 100, 0.04)
+        Sound.play(Sound.MAC_OS_OLD_SCHOOL_ERROR_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_1_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_2_SOUND)
+        Sound.play(Sound.MAC_OS_OLD_SCHOOL_ERROR_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_1_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_2_SOUND)
+        Sound.play(Sound.MAC_OS_OLD_SCHOOL_ERROR_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_1_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_2_SOUND)
+        Sound.play(Sound.MAC_OS_OLD_SCHOOL_ERROR_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_1_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_2_SOUND)
+        Sound.play(Sound.MAC_OS_OLD_SCHOOL_ERROR_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_1_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_2_SOUND)
+        Sound.play(Sound.MAC_OS_OLD_SCHOOL_ERROR_SOUND)
+        Sound.play(Sound.MAC_OS_ERROR_1_SOUND)
+
+# Write code in main function below to tes tthis file on its own:
+if __name__ == '__main__':
+    def main():
+        pass
+
+    main()
