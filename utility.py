a814a6fd562f7338215fd7b80b655b6d3cee74b4
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-02-09 23:18:10 -0700

    add clear_line, hide cursor, show_cursor methods and add Thread Control for threading animations and sound

--- utility.py
+++ utility.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-import os
+import os, sys
+import threading
+from time import sleep
 
 class Utility:
     @staticmethod
@@ -14,4 +16,34 @@ class Utility:
     def clear_screen():
         os.system('cls' if os.name == 'nt' else 'clear')
     
+    @staticmethod
+    def clear_line():
+        sys.stdout.write("\033[2K\r")
+        sys.stdout.flush()
     
+    @staticmethod
+    def hide_cursor():
+        print("\033[?25l", end="")
+    
+    @staticmethod
+    def show_cursor():
+        print("\033[?25h", end="")
+    
+
+class ThreadControl:
+    def __init__(self, target, *args):
+        self.target = target
+        self.args = args
+        self.stop_event = threading.Event()
+        self.thread = threading.Thread(target = self._run)
+    
+    def _run(self):
+        self.target(self.stop_event)
+    
+    def start(self):
+        self.thread.start()
+    
+    def stop(self, hold_thread_for = 0.0):
+        self.stop_event.set()
+        self.thread.join()
+        sleep(hold_thread_for)
