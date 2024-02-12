918cc1fe6c6fcf4cc7b275204f744b10218cf86e
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-02-12 11:21:25 -0700

    modify method names in Animation class

--- animation.py
+++ animation.py
@@ -6,8 +6,8 @@ from utility import Utility, ThreadControl
 class Animation:
     
     @staticmethod
-    def text_animating_after_static_text(static_text: str, animated_text: str = "...", end_text:str = "", delay_animation: float = 0.1, stop_event = None, hold_thread_for = 0.0):
-        def text_animating_after_static_text_thread(stop_event=None):
+    def animated_text(static_text: str, animated_text: str = "...", end_text:str = "", delay_animation: float = 0.1, stop_event = None, hold_thread_for = 0.0):
+        def animated_text_thread(stop_event=None):
             while True:
                 if stop_event and stop_event.is_set():
                     break
@@ -19,7 +19,7 @@ class Animation:
                     print(static_text + animated_text[:i+1], end="")
                     sleep(delay_animation)
             print("", end=end_text)
-        animation_thread = ThreadControl(text_animating_after_static_text_thread, stop_event)
+        animation_thread = ThreadControl(animated_text_thread, stop_event)
         animation_thread.start()
         sleep(hold_thread_for)
         return animation_thread
