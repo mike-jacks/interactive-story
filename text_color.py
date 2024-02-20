d9ca458d9ae552a13b6db1e83a4e4494af490a45
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-02-20 15:17:58 -0700

    add doc string to text_color.py

--- text_color.py
+++ text_color.py
@@ -1,10 +1,23 @@
 from enum import Enum
 from random import choice
 
-def get_random_color():
-    return choice(TextColor.RAINBOW.value)
-
 class TextColor(Enum):
+    """
+    An enumeration for different text colors using ANSI escape codes.
+
+    Attributes:
+        BLACK (str): ANSI code for black color.
+        RED (str): ANSI code for red color.
+        ORANGE (str): ANSI code for orange color.
+        YELLOW (str): ANSI code for yellow color.
+        GREEN (str): ANSI code for green color.
+        BLUE (str): ANSI code for blue color.
+        CYAN (str): ANSI code for cyan color.
+        PURPLE (str): ANSI code for purple color.
+        WHITE (str): ANSI code for white color.
+        RESET (str): ANSI code to reset color to default.
+        RAINBOW (list): List of ANSI codes for a spectrum of colors.
+    """
     BLACK = '\033[30m'
     RED = '\033[38;5;9m'
     ORANGE = '\033[38;5;208m'
@@ -19,6 +32,12 @@ class TextColor(Enum):
     
     @classmethod
     def random(cls):
+        """
+        Selects and returns a random color from the TextColor enum, excluding the RAINBOW list itself.
+
+        Returns:
+            TextColor: An instance of the TextColor enum representing a random color.
+        """
         return choice([color for color in cls if color != cls.RAINBOW])
 
 
