d8fab6c279afd39aaed4f86b8f04b4fd0a8115cd
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-02-20 15:15:19 -0700

    add doc string to sound.py

--- sound.py
+++ sound.py
@@ -3,6 +3,14 @@ import threading
 from playsound import playsound
 
 class Sound:
+    """
+    A class to manage and play sound files for various system and custom notifications.
+
+    Attributes:
+        Constants representing file paths to various sound files.
+    """
+    
+    # Sound file paths
     MAC_OS_ERROR_1_SOUND = 'sounds/mac_os_error_1.wav'
     MAC_OS_ERROR_2_SOUND = 'sounds/mac_os_error_2.wav'
     MAC_OS_NOTIFICAITON_1_SOUND = 'sounds/mac_os_notification_1.wav'
@@ -21,6 +29,14 @@ class Sound:
     
     @staticmethod
     def play(sound_file: str = MAC_OS_STARTUP_MODERN_SOUND, loop: int = 1, pause: float = 0.0):
+        """
+        Plays the specified sound file a given number of times with a pause between each play.
+
+        Parameters:
+            sound_file (str): The path to the sound file to be played.
+            loop (int): The number of times the sound file is played.
+            pause (float): The duration (in seconds) to wait between each play.
+        """
         def sound_thread():
             playsound(sound_file)
         for i in range(loop):
@@ -29,6 +45,9 @@ class Sound:
     
     @staticmethod
     def crash():
+        """
+        Simulates a system crash sound sequence by playing a series of error sounds and startup sounds.
+        """
         Sound.play(Sound.MAC_OS_STARTUP_CLASSIC_SOUND)
         Sound.play(Sound.MAC_OS_ERROR_1_SOUND, 2, 0)
         Sound.play(Sound.MAC_OS_ERROR_2_SOUND, 2, 0)
@@ -70,9 +89,3 @@ class Sound:
         Sound.play(Sound.MAC_OS_OLD_SCHOOL_ERROR_SOUND)
         Sound.play(Sound.MAC_OS_ERROR_1_SOUND)
 
-# Write code in main function below to tes tthis file on its own:
-if __name__ == '__main__':
-    def main():
-        pass
-
-    main()
