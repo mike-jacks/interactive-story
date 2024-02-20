389e7dc487f63a89e107958ea6d0e8cafc2ea2e9
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-02-20 11:57:18 -0700

    add doc string to animation.py

--- animation.py
+++ animation.py
@@ -2,12 +2,38 @@ from time import sleep
 from utility import Utility, ThreadControl
 from text_color import TextColor
 from random import choice
-
 class Animation:
+    """
+    This class provides methods to create text animations in the console.
+    It uses various text colors and can animate the text with a typing effect.
+    """
 
     @staticmethod
     def animated_text(static_text: str = "", animated_text: str = "...", end_text:str = "", static_text_color: TextColor = TextColor.RESET, animated_text_color: TextColor = TextColor.RESET, end_text_color: TextColor = TextColor.RESET, delay_between_chars: float = 0.1, stop_event = None, continue_thread_after_stop_for = 0.001):
+        """
+        Prints animated text to the console with specified colors and delays.
+
+        Parameters:
+        - static_text (str): Text to remain static before the animated text.
+        - animated_text (str): Text to be animated, like a loading spinner.
+        - end_text (str): Text to display after the animation ends.
+        - static_text_color (TextColor): Color of the static text.
+        - animated_text_color (TextColor): Color of the animated text.
+        - end_text_color (TextColor): Color of the end text.
+        - delay_between_chars (float): Time in seconds between each character's animation.
+        - stop_event (threading.Event): Event to stop the animation thread externally.
+        - continue_thread_after_stop_for (float): Time in seconds to continue the thread after a stop event is set.
+
+        Returns:
+        ThreadControl: A thread control object for the animation thread.
+        """
         def animated_text_thread(stop_event=None):
+            """
+            Inner function that runs the animation loop. This function is designed to be run in a separate thread to allow asynchronous text animation.
+
+            Parameters:
+            - stop_event (threading.Event): Event to stop the animation thread externally.
+            """
             while True:
                 if stop_event and stop_event.is_set():
                     break
